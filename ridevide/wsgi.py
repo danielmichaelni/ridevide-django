d6afd23b8001f3d92dc8b17479bb3b1d1e2dd85e
Daniel Ni <danielmichaelni@gmail.com>
2014-07-26 21:31:50 -0400

    my django app

---
+++ ridevide/wsgi.py
@@ -0,0 +1,21 @@
+"""
+WSGI config for ridevide project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+"""
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ridevide.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
+
+# HEROKU STUFF
+
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
