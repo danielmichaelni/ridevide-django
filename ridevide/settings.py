cf3ce224a21fb4ba08b9d20bb019cd33bef6a2b5
Daniel Ni <danielmichaelni@gmail.com>
2014-09-06 23:58:42 -0400

    turn off debug

--- ridevide/settings.py
+++ ridevide/settings.py
@@ -20,9 +20,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = '1i)o7cnf**l1ntr9i*c!2x3$t58g8l8qwgv+x-^ifnziu=fur)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = False
 
 ALLOWED_HOSTS = []
 
