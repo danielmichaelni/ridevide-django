699eeb754128e35717e07588b65dbfa6dd4c9d5c
Daniel Ni <danielmichaelni@gmail.com>
2014-09-07 00:05:18 -0400

    turn debug back on

--- ridevide/settings.py
+++ ridevide/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = '1i)o7cnf**l1ntr9i*c!2x3$t58g8l8qwgv+x-^ifnziu=fur)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 TEMPLATE_DEBUG = True
 
